9515b9b9400f10faaba4592dd7f1ed74a7459b71
romer8 <gio.busrom@gmail.com>
2023-01-12 01:25:30 +0000

    tag version v1.0.12_2023_01_12

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-from setuptools import setup, find_namespace_packages
-from setup_helper import find_resource_files
-from tethys_apps.base.app_base import TethysAppBase
-
-# -- Apps Definition -- #
-app_package = 'test_app_store_aquaveo'
-release_package = f'{TethysAppBase.package_namespace}-{app_package}'
-
-# -- Python Dependencies -- #
-dependencies = []
-
-# -- Get Resource File -- #
-resource_files = find_all_resource_files(app_package, TethysAppBase.package_namespace)
-
-
-resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' +                     app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' +                     app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' +                     app_package)
-setup(
+(Pdb) (Pdb) (Pdb) (Pdb) from setuptools import setup, find_namespace_packages
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) from setup_helper import find_resource_files
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) from tethys_apps.base.app_base import TethysAppBase
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) 
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) # -- Apps Definition -- #
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) (Pdb) app_package = 'test_app_store_aquaveo'
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) release_package = f'{TethysAppBase.package_namespace}-{app_package}'
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) 
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) # -- Python Dependencies -- #
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) dependencies = []
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) 
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) # -- Get Resource File -- #
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) resource_files = find_all_resource_files(app_package, TethysAppBase.package_namespace)
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) 
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) 
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' +                     app_package)
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' +                     app_package)
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' +                     app_package)
+(Pdb) (Pdb) (Pdb) (Pdb) (Pdb) (Pdb) (Pdb) (Pdb) resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' +                     app_package)
+(Pdb) setup(
     name=release_package,
     version='0.0.1',
     description='This is my testing app for submission and installation',
