c9d7c0c39ad0e31b330f9c63a1e78291ff17ce36
romer8 <gio.busrom@gmail.com>
2023-01-12 10:12:32 -0700

    new5

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_namespace_packages
-from setup_helper import find_all_resource_files
+from setup_helper import find_resource_files
 
 
 # -- Apps Definition -- #
-
-namespace = 'tethysapp'
 app_package = 'test_app_store_aquaveo'
-release_package = f'{namespace}-{app_package}'
+release_package = 'tethysapp-'+ app_package
 
 
 # -- Python Dependencies -- #
 dependencies = []
 
 # -- Get Resource File -- #
-resource_files = find_all_resource_files(app_package, namespace)
+resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
+resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
+resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
+
+resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' + app_package)
 
 
 
